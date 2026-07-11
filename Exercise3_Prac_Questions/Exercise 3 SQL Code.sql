9e1ea678ce8df23db0911a2459d5cf1143dc0337
lisoletu <lisoletudigala@gmail.com>
2026-07-11 20:11:38 +0000

    adding changes

---
+++ Exercise3_Prac_Questions/Exercise 3 SQL Code.sql
@@ -0,0 +1,78 @@
+-- Databricks notebook source
+--Running the full tables before analysis to see what I have in my data
+--------------------------------------------------------------------------
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.attendance;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.classes;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.deliveries;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.employeess;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.orders;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.payments;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.products;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.products_inventory;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.students;
+
+SELECT *
+FROM case_statements_prac_exercise3.exercise_data.tickets;
+
+
+--1. Table: products 
+--------------------------------------------------------
+
+
+--2. Table: orders
+--------------------------------------------------------
+
+
+--3. Table: employees
+--------------------------------------------------------
+
+
+--4. Table: students
+--------------------------------------------------------
+
+
+--5. Table: deliveries
+-------------------------------------------------------
+
+
+
+--6. Table: tickets 
+-----------------------------------------------------------
+
+
+--7. Table: attendance
+-----------------------------------------------------------
+
+
+
+--8. Table: products_inventory
+-----------------------------------------------------------
+
+
+
+--9. Table: classes
+-----------------------------------------------------------
+
+
+--10. Table: payments 
+----------------------------------------------------------
+
+
+
